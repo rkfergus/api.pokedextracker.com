5e59a8daa7dc9a634d3288ee39b3ccbde85089d0
Robin Joseph <robin@lob.com>
2016-11-05 14:29:28 -0700

    feat(dexes): add caught and total counts to dexes

--- src/models/dex.js
+++ src/models/dex.js
@@ -1,20 +1,44 @@
 'use strict';
 
 const Bookshelf = require('../libraries/bookshelf');
+const Knex      = require('../libraries/knex');
+
+const TOTALS = {
+  1: 151,
+  2: 251,
+  3: 386,
+  4: 493,
+  5: 649,
+  6: 721
+};
 
 module.exports = Bookshelf.model('Dex', Bookshelf.Model.extend({
   tableName: 'dexes',
   hasTimestamps: ['date_created', 'date_modified'],
+  caught () {
+    return Knex('captures').count().where('dex_id', this.get('id'))
+    .then((res) => parseInt(res[0].count));
+  },
+  virtuals: {
+    total () {
+      return TOTALS[this.get('generation')];
+    }
+  },
   serialize () {
-    return {
-      id: this.get('id'),
-      user_id: this.get('user_id'),
-      title: this.get('title'),
-      slug: this.get('slug'),
-      shiny: this.get('shiny'),
-      generation: this.get('generation'),
-      date_created: this.get('date_created'),
-      date_modified: this.get('date_modified')
-    };
+    return this.caught()
+    .then((caught) => {
+      return {
+        id: this.get('id'),
+        user_id: this.get('user_id'),
+        title: this.get('title'),
+        slug: this.get('slug'),
+        shiny: this.get('shiny'),
+        generation: this.get('generation'),
+        caught,
+        total: this.get('total'),
+        date_created: this.get('date_created'),
+        date_modified: this.get('date_modified')
+      };
+    });
   }
 }));
--- test/models/dex.test.js
+++ test/models/dex.test.js
@@ -1,24 +1,72 @@
 'use strict';
 
-const Dex = require('../../src/models/dex');
+const Bluebird = require('bluebird');
 
-const dex = Factory.build('dex');
+const Dex  = require('../../src/models/dex');
+const Knex = require('../../src/libraries/knex');
+
+const user = Factory.build('user');
+
+const firstPokemon  = Factory.build('pokemon', { national_id: 1 });
+const secondPokemon = Factory.build('pokemon', { national_id: 2 });
+
+const dex = Factory.build('dex', { user_id: user.id });
+
+const firstCapture  = Factory.build('capture', { pokemon_id: firstPokemon.national_id, user_id: user.id, dex_id: dex.id });
+const secondCapture = Factory.build('capture', { pokemon_id: secondPokemon.national_id, user_id: user.id, dex_id: dex.id });
 
 describe('dex model', () => {
+
+  describe('caught', () => {
+
+    beforeEach(() => {
+      return Bluebird.all([
+        Knex('users').insert(user),
+        Knex('pokemon').insert([firstPokemon, secondPokemon])
+      ])
+      .then(() => Knex('dexes').insert(dex))
+      .then(() => Knex('captures').insert([firstCapture, secondCapture]));
+    });
+
+    it('returns the count of captures associated with this dex', () => {
+      return Dex.forge(dex).caught()
+      .then((caught) => {
+        expect(caught).to.eql(2);
+      });
+    });
+
+  });
+
+  describe('virtuals', () => {
+
+    describe('total', () => {
+
+      it('gets the total pokemon in a generation', () => {
+        expect(Dex.forge({ generation: 6 }).get('total')).to.eql(721);
+      });
+
+    });
+
+  });
 
   describe('serialize', () => {
 
     it('returns the correct fields', () => {
-      expect(Dex.forge(dex).serialize()).to.have.all.keys([
-        'id',
-        'user_id',
-        'title',
-        'slug',
-        'shiny',
-        'generation',
-        'date_created',
-        'date_modified'
-      ]);
+      return Dex.forge(dex).serialize()
+      .then((json) => {
+        expect(json).to.have.all.keys([
+          'id',
+          'user_id',
+          'title',
+          'slug',
+          'shiny',
+          'generation',
+          'caught',
+          'total',
+          'date_created',
+          'date_modified'
+        ]);
+      });
     });
 
   });
