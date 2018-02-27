521d60dd523dab631b1b18d3418f6a99b821435d
Robin Joseph <robinmjoseph08@gmail.com>
2018-02-26 21:05:27 -0800

    chore(db): drop generation and region from pokemon and dexes (#98)

--- test/factories/pokemon.js
+++ test/factories/pokemon.js
@@ -5,5 +5,4 @@ module.exports = Factory.define('pokemon')
   .sequence('national_id')
   .sequence('evolution_family_id')
   .attr('name', '')
-  .attr('generation', 1)
   .sequence('national_order');
