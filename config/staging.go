d320723e1a754d986a80bf90b40c24889ac21c5d
Robin Joseph <robin@lob.com>
2018-08-01 20:52:57 -0700

    read from DATABASE_PASSWORD

--- config/staging.go
+++ config/staging.go
@@ -7,6 +7,7 @@ import (
 func loadStagingConfig(cfg *Config) {
 	cfg.DatabaseHost = os.Getenv("DATABASE_HOST")
 	cfg.DatabaseName = os.Getenv("DATABASE_NAME")
+	cfg.DatabasePassword = os.Getenv("DATABASE_PASSWORD")
 	cfg.DatabaseSSLMode = os.Getenv("DATABASE_SSL_MODE")
 	cfg.DatabaseUser = os.Getenv("DATABASE_USER")
 	cfg.Environment = "staging"
